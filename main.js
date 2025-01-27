d3eff6291ec59880a493132d654ebb2e20a1b17d
Riccardo Gucciardo <riccardogucciardo003@gmail.com>
2025-01-27 17:02:04 +0100

    secondo esercizio

--- main.js
+++ main.js
@@ -24,3 +24,48 @@ for( let i = 1; i < biciDaCorsa.length; i++ ) {
 }
 console.log(biciPiuleggera);
 
+
+//Snack2
+//Creare un array di oggetti di squadre di calcio.
+//Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+//Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+//Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+//Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const squadreCalcio = [
+    { nome: "Juventus", punti: 0, falliSubiti: 0 },
+    { nome: "Milan", punti: 0, falliSubiti: 0 },
+    { nome:"Inter", punti: 0, falliSubiti: 0 }, 
+    { nome: "Napoli", punti: 0, falliSubiti: 0},
+    { nome:"Roma", punti: 0, falliSubiti: 0} 
+];
+
+function numRandom(min, max) {
+    return Math.floor(Math.random() * max) + min
+}
+
+for (const index in squadreCalcio) {
+    console.log( index, squadreCalcio[index] )
+    
+    let elemento = squadreCalcio[index]
+
+    elemento.punti = numRandom(1, 50)
+    elemento.falliSubiti = numRandom(1, 50)
+}
+
+let nuovaArrey = []
+
+for (const index in squadreCalcio) {
+
+let elemento = squadreCalcio[index]
+
+elemento.punti = numRandom(1, 50)
+elemento.falliSubiti = numRandom(1, 50)
+
+nuovaArrey.push(
+    {
+        nome: elemento["nome"],
+        falliSubiti: elemento["falliSubiti"]
+    }
+)
+}
