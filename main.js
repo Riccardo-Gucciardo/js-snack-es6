fb3681ab735e8b353dce000abc1b280ac60faf2b
Riccardo Gucciardo <riccardogucciardo003@gmail.com>
2025-01-27 15:37:22 +0100

    es n°1

---
+++ main.js
@@ -0,0 +1,24 @@
+//Snack 1
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore.
+
+const biciDaCorsa =[
+{nome:"Speedster 3000", peso: 7.5 }, //8
+{nome:"Lightning Pro", peso: 6.8 },
+{nome:"Carbon x", peso: 7.2 },
+{ nome:"Aero Racer", peso: 7.9 },
+{ nome: "Mountain Climber", peso: 6.5 }
+]
+
+let biciPiuleggera = biciDaCorsa[0]
+
+
+for( let i = 1; i < biciDaCorsa.length; i++ ) {
+    
+    let elemento = biciDaCorsal[i]
+    if( elemento. peso < biciPiuLeggera. peso ) {
+    
+        biciPiuLeggera = elemento
+    }
+}
